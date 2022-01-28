7182727e15f6fbcf05d5fbf9f082a268dfaaf5dc
Ronaldo Macapobre <ronaldosmacapobre@gmail.com>
2022-01-28 15:04:59 +0800

    initial commit wip

--- easybank-landing-page-master/script.js
+++ easybank-landing-page-master/script.js
@@ -1,1 +1,17 @@
-console.log('ronnie');
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', (e) => {
+  console.log('hamburger clicked');
+
+  hamburger.classList.toggle('close');
+  close.classList.toggle('close');
+  overlay.classList.toggle('close');
+});
+
+close.addEventListener('click', (e) => {
+  hamburger.classList.toggle('close');
+  close.classList.toggle('close');
+  overlay.classList.toggle('close');
+});
